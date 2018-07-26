55c861db0f54b02a0d4657e12ca5ca7a998a1930
Harshil <harshilpk@gmail.com>
2018-07-26 16:55:06 -0700

    Basics

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  templateUrl: './post-create.component.html',
+  selector: 'app-post-create',
+})
+export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'No Content';
+
+  onAddPost() {
+
+    this.newPost = this.enteredValue;
+  }
+}
