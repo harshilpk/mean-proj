b3e16411ab6b223e0a42e9f7fc7262e1d5cf63c6
Harshil <harshilpk@gmail.com>
2018-07-26 18:13:24 -0700

    Angular Material added

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 @Component({
   templateUrl: './post-create.component.html',
   selector: 'app-post-create',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
   enteredValue = '';
